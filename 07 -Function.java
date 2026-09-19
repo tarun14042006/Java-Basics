995b9ad43c82e012efa64785c03cdcc748b9be04
Tarun Gohil <trgohil2006@gmail.com>
2026-09-19 12:58:34 +0530

    Basic function example

---
+++ 07 -Function.java
@@ -0,0 +1,10 @@
+public class Functions {
+
+    static void greet() {
+        System.out.println("Hello, welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
