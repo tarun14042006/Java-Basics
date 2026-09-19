7ff6d601b68a0797f74bce698ebd7526ae0deebd
Tarun Gohil <trgohil2006@gmail.com>
2026-09-19 10:46:56 +0530

    Add Variables.java with basic variable declarations

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class variable{
+  public static void main(String arg[]){
+
+    int age = 20;
+    double marks = 89.8;
+    char grade = 'A';
+    String name = "Tarun";
+    boolean passed = true;
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Marks: " + marks);
+    System.out.println("Grade: " + grade);
+    System.out.println("Passed: " + passed);
+  }
+}
+    
